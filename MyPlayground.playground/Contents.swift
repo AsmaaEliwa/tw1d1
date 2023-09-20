fda44349898689da3b8a5c5f0e479d8702ea2c24
Asmaa Eliwa <asmaagamalelsayed96@gmail.com>
2023-09-20 09:46:51 -0400

    classes

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -4,7 +4,7 @@ var greeting = "Hello, playground"
 //let myName = "asmaa"
 
 func printName( name: String){
-    for i in 0..<22{
+    for _ in 0..<22{
         print(name)
 
     }
@@ -21,5 +21,71 @@ var array = [2,1,5,3,8,6,9,0,12]
 //array.sort()
 //array.reverse()
 //array.append(3)
-array.insert(7, at: 3)
-print(array)
+//array.last
+//array.insert(7, at: 3)
+//print(array)
+
+
+var arr : [Int]=[1]
+var last = arr.last
+//var last =  arr.last ?? 3
+
+
+if let ele=last {
+    print("the last ele is \(ele)")
+}else{
+    print("you have no studients")
+}
+
+
+
+
+class Car{
+    var name: String?
+    var price: Int?
+    init(){}
+    init(name: String,  price: Int){
+        self.name = name
+        self.price = price
+    }
+    func sound(){
+        print("hhhhhhh")
+    }
+
+}
+
+let bm = Car(name: "BM", price: 27000)
+bm.name
+
+let ww=Car()
+ww.sound()
+
+class Developer{
+    var name: String
+    var age: Int
+    
+    init(name: String,  age: Int){
+        self.name=name
+        self.age=age
+    }
+    func sayName(){
+        print(name)
+    }
+
+}
+
+let firstDeceloper = Developer(name: "asmaa", age: 27)
+firstDeceloper.sayName()
+
+class iosDeveloper: Developer {
+    var frameWork: String?
+    override func sayName() {
+        print("\(name) is an IOS developer")
+    }
+    
+}
+
+let ali = iosDeveloper(name:"ali", age: 33)
+
+
+ali.sayName()
